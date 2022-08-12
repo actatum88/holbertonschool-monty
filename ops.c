3b7dc9f162c66bdbc869aa6e76ed7777b3dcf7a7
Allie Tatum <102764058+actatum88@users.noreply.github.com>
2022-08-11 22:43:15 -0500

    Update ops.c

--- ops.c
+++ ops.c
@@ -10,9 +10,9 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || *stack == NULL)
+	if (*stack == NULL)
 	{
-		printf("L%u: can't pint, stack empty\n", line_number);
+		fprintf("L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
