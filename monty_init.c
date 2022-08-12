271b73e02dea377cd50873dbfdf44c205a68dac3
actatum88 <4946@holbertonschool.com>
2022-08-11 21:34:33 -0700

    More memory leaks

--- monty_init.c
+++ monty_init.c
@@ -5,12 +5,15 @@ FILE *monty_init(int ac, char *av[])
 
 /*Check for correct usage.*/
 	if (ac != 2)
-		fprintf(stderr, "USAGE: %s file\n", av[0]), exit(EXIT_FAILURE);
-	/*Check whether the file exists and is readable.*/
+	{
+		fprintf(stderr, "USAGE: %s file\n", av[0]);
+		exit(EXIT_FAILURE);
+	}
+/*Check whether the file exists and is readable.*/
 	if (access(av[1], F_OK | R_OK))
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-			exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	/*If you argued correctly, the file is opened*/
 	data.script = temp = fopen(av[1], "r");
