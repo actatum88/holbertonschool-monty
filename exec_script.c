d5ea2aeb875e5a669c5ebde1130875ba0d1fdc94
magicaldave <smokydave@icloud.com>
2022-08-12 10:32:44 -0500

    Edit: handled malloc after getline

--- exec_script.c
+++ exec_script.c
@@ -11,11 +11,19 @@ void exec_script(stack_t **stack)
 	stack_t *temp;
 	void (*f)(stack_t **stack, unsigned int line_number);
 
+	data.buf = NULL;
+
 	/*Read each line of your Monty script*/
 	while (getline(&data.buf, &nchars, data.script) != EOF && ++line)
 	{
+		if (!data.buf)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			fclose(data.script);
+			exit(EXIT_FAILURE);
+		}
 		squeeze_spaces(data.buf); /*Sanitize the input*/
-		if (*(data.buf))
+		if (*(data.buf) && *(data.buf) != '\n')
 		{
 			f = get_op(strtok(data.buf, &delim)); /*Get the first word as a function*/
 			if (f)
